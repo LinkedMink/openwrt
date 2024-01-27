4ecc994fd3fd428956a13c4103df9466ade1ad31
Harlan Sang <harlan.sang@linkedmink.net>
2024-01-27 09:55:35 -0600

    Add swig as required package for Ubuntu setup

--- lm-ubuntu-setup.sh
+++ lm-ubuntu-setup.sh
@@ -16,7 +16,7 @@ sudo apt update
 sudo apt install \
     build-essential clang flex bison g++ gawk gcc-multilib g++-multilib \
     gettext git libncurses-dev libssl-dev python3-distutils rsync unzip zlib1g-dev \
-    file wget
+    file wget swig
 
 logInfo "Setup build configuration for forked repo"
 
